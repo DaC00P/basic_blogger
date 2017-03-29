932a9fd59af7bf513841b9acebd7974c77b1dade
Daniel Cuperman <dacooperfish@gmail.com>
2017-03-29 10:25:06 -0700

    new post functionality functioning

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,17 @@ class PostsController < ApplicationController
 
   before_action :select_post, only: [:edit, :update, :show, :destroy, :sold]
 
+  def new
+    @post = Post.new
+  end
+
   def index
     @posts = Post.all
   end
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
 
     if @post.save
       flash[:notice] = 'Post was successfully created.'
