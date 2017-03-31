b53cbda1d98fef03fc84ad3bc8032fe3bcbb0103
Daniel Cuperman <dacooperfish@gmail.com>
2017-03-30 17:07:23 -0700

    index posts controller tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,20 +3,25 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
   include Devise::Test::ControllerHelpers
   describe 'GET #index' do
-    context 'when user is logged in' do
-      user = FactoryGirl.build(:user)
-      before do
-        sign_in user
+    context 'when user is not logged in' do
+      it 'should redirect to sign in page' do
         get :index
+        should use_before_action(:authenticate_user!)
+        should redirect_to('/users/sign_in')
       end
-
-      
     end
-    context 'when user is logged out' do
-      pending
+
+    context 'when user is logged in' do
+      it 'should redirect them to /posts/index' do
+        user = FactoryGirl.build(:user)
+        sign_in(user)
+        should use_before_action(:authenticate_user!)
+        assert_response :success
+      end
     end
   end
 end
+
 # test "logged in should get show" do
 #   sign_in users(:one)
 #   get :show
