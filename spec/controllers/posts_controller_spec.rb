3676db31bfb26ea054b0a591f05cc3a920bbb58b
Daniel Cuperman <dacooperfish@gmail.com>
2017-03-30 20:08:49 -0700

    turns out RSPEC wasn't configured properly to hit routes. re-writing post controller tests with that in mind

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -12,38 +12,23 @@ RSpec.describe PostsController, type: :controller do
       it 'should redirect to sign in page when the access the index' do
         should redirect_to('/users/sign_in')
       end
+
+      it 'should NOT create a post' do
+        @post = FactoryGirl.build(:post)
+        expect do
+          post :create, post: { content: @post.content, title: @post.title }
+        end.to_not change { Post.all.length }
+        assert_response 302
+        should redirect_to('/users/sign_in')
+      end
     end
 
     context 'when user is logged in' do
-      before do
-        user = FactoryGirl.build(:user)
-        sign_in(user)
-        should use_before_action(:authenticate_user!)
-      end
-
-      it 'should redirect them to /posts/index' do
-        assert_response :success
-        redirect_to 'posts'
-      end
-
-      it 'should create a post' do
-        expect do
-          Post.create!(title: 'a', content: 'b', user_id: 3)
-        end.to change { Post.all.length }
-        assert_response :success
-        redirect_to 'posts'
-      end
+        before do
+          @user = FactoryGirl.build(:user)
+          sign_in(@user)
+          should use_before_action(:authenticate_user!)
+        end
     end
   end
 end
-
-# test "logged in should get show" do
-#   sign_in users(:one)
-#   get :show
-#   assert_response :success
-# end
-#
-# test "not authenticated should get redirect" do
-# get :show
-# assert_response :redirect
-# end
