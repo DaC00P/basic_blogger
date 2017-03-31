97c8aece13c7aa02e9b261d6f971026827906b2d
Daniel Cuperman <dacooperfish@gmail.com>
2017-03-30 20:33:32 -0700

    was overtesting, needed to use Capybara to realistically visit page then imitate post as a user, etc

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
   include Devise::Test::ControllerHelpers
   describe 'Posts Controller' do
-    context 'when user is not logged in' do
+    describe 'when user is not logged in' do
       it 'should redirect to sign in page when the access the index' do
         get :index
         should use_before_action(:authenticate_user!)
@@ -11,13 +11,7 @@ RSpec.describe PostsController, type: :controller do
       end
     end
 
-    context 'when user is logged in' do
-        before do
-          @user = FactoryGirl.build(:user)
-          sign_in(@user)
-          should use_before_action(:authenticate_user!)
-        end
-
+    describe 'post creation' do
         it 'should create a post' do
           @post = Post.new(id: 1, title: 'a', content: 'b', user_id: 1)
           expect do
