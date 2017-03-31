040eba3cf632da9c12f1c86fef4c59fa0c21280e
Daniel Cuperman <dacooperfish@gmail.com>
2017-03-30 20:27:09 -0700

    posts controller specs still under construction

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,23 +2,11 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
   include Devise::Test::ControllerHelpers
-  describe 'GET #index' do
+  describe 'Posts Controller' do
     context 'when user is not logged in' do
-      before do
+      it 'should redirect to sign in page when the access the index' do
         get :index
         should use_before_action(:authenticate_user!)
-      end
-
-      it 'should redirect to sign in page when the access the index' do
-        should redirect_to('/users/sign_in')
-      end
-
-      it 'should NOT create a post' do
-        @post = FactoryGirl.build(:post)
-        expect do
-          post :create, post: { content: @post.content, title: @post.title }
-        end.to_not change { Post.all.length }
-        assert_response 302
         should redirect_to('/users/sign_in')
       end
     end
@@ -28,6 +16,14 @@ RSpec.describe PostsController, type: :controller do
           @user = FactoryGirl.build(:user)
           sign_in(@user)
           should use_before_action(:authenticate_user!)
+        end
+
+        it 'should create a post' do
+          @post = Post.new(id: 1, title: 'a', content: 'b', user_id: 1)
+          expect do
+            post :create, post: @post
+          end
+          assert_response :success
         end
     end
   end
