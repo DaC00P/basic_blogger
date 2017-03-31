5379c6ee6cd31047661e63123dd397dee97f9550
Daniel Cuperman <dacooperfish@gmail.com>
2017-03-30 17:30:03 -0700

    post creation testing

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,19 +4,34 @@ RSpec.describe PostsController, type: :controller do
   include Devise::Test::ControllerHelpers
   describe 'GET #index' do
     context 'when user is not logged in' do
-      it 'should redirect to sign in page' do
+      before do
         get :index
         should use_before_action(:authenticate_user!)
+      end
+
+      it 'should redirect to sign in page when the access the index' do
         should redirect_to('/users/sign_in')
       end
     end
 
     context 'when user is logged in' do
-      it 'should redirect them to /posts/index' do
+      before do
         user = FactoryGirl.build(:user)
         sign_in(user)
         should use_before_action(:authenticate_user!)
+      end
+
+      it 'should redirect them to /posts/index' do
         assert_response :success
+        redirect_to 'posts'
+      end
+
+      it 'should create a post' do
+        expect do
+          Post.create!(title: 'a', content: 'b', user_id: 3)
+        end.to change { Post.all.length }
+        assert_response :success
+        redirect_to 'posts'
       end
     end
   end
